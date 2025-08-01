dab0cb21b5c77c3dbf4a732eff6f0a30e95eb398
Benkralex <75564514+Benkralex@users.noreply.github.com>
2025-08-01 11:30:42 +0200

    Update Gradle and library versions; clean up

--- composeApp/src/commonMain/kotlin/de/benkralex/partygames/games/common/presentation/StringListInputWidget.kt
+++ composeApp/src/commonMain/kotlin/de/benkralex/partygames/games/common/presentation/StringListInputWidget.kt
@@ -1,13 +1,10 @@
 package de.benkralex.partygames.games.common.presentation
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
